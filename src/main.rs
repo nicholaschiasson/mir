09a78867508339911d452be025c1da210580dadb
Nicholas Omer Chiasson <nicholasomerchiasson@gmail.com>
2018-12-14 18:25:51 -0500

    Some refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use gitlab::Gitlab;
-use rpassword::prompt_password_stdout;
+use rpassword;
 use structopt::StructOpt;
 
-use std::fs::create_dir_all;
+use std::collections::HashSet;
+use std::fs;
 
 /// A tool to mirror a GitLab user's group hierarchy on the local file system
 #[derive(StructOpt, Debug)]
@@ -24,14 +25,12 @@ struct CliArgs {
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = CliArgs::from_args();
-    let password = prompt_password_stdout("Enter GitLab personal access token: ")?;
+    let password = rpassword::prompt_password_stdout("Enter GitLab personal access token: ")?;
     let gitlab = Gitlab::new(&args.host, password)?;
     let projects = gitlab.owned_projects()?;
-    let mut namespaces = projects.iter().map(|ref p| &p.namespace.full_path).collect::<Vec<_>>();
-    namespaces.dedup();
-    namespaces.iter().for_each(|n| {
+    projects.iter().map(|ref p| &p.namespace.full_path).collect::<HashSet<_>>().iter().for_each(|n| {
         let namespace = format!("{}/{}", args.destination, n);
-        create_dir_all(&namespace).expect(&format!("failed to create the directory '{}'", n));
+        fs::create_dir_all(&namespace).expect(&format!("failed to create the directory '{}'", n));
     });
     Ok(())
 }
