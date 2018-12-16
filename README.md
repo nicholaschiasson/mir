7ed0b68f8beef543aa33bb1ecbe64c3399a1954d
Nicholas Omer Chiasson <nicholasomerchiasson@gmail.com>
2018-12-15 20:28:25 -0500

    Feature complete and adding ci

--- README.md
+++ README.md
@@ -1,4 +1,25 @@
 # mir
 
-Tool to mirror a user's entire GitLab group hierarchy locally with the option
-to clone all projects.
+Tool to mirror a user's entire owned GitLab group hierarchy locally and
+optionally clone all projects
+
+```
+mir
+Nicholas Omer Chiasson <nicholasomerchiasson@gmail.com>
+Tool to mirror a user's entire owned GitLab group hierarchy locally and optionally clone all projects
+
+USAGE:
+    mir [FLAGS] [OPTIONS]
+
+FLAGS:
+    -c, --clone      Clone all repositories
+    -h, --help       Prints help information
+    -V, --version    Prints version information
+
+OPTIONS:
+    -d, --destination <destination>
+            The destination directory in which the hierarchy should be mirrored [default: .]
+
+    -H, --host <host>                                      GitLab remote host [default: gitlab.com]
+    -p, --personal-access-token <personal_access_token>    GitLab personal access token
+```
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
+use git2::{build::RepoBuilder, Cred, CredentialType, Error, FetchOptions, RemoteCallbacks};
 use gitlab::Gitlab;
+use indicatif::{ProgressBar, ProgressStyle};
 use rpassword;
 use structopt::StructOpt;
 
 use std::collections::HashSet;
 use std::fs;
+use std::path::Path;
 
-/// A tool to mirror a GitLab user's group hierarchy on the local file system
+/// Tool to mirror a user's entire owned GitLab group hierarchy locally and optionally clone all projects
 #[derive(StructOpt, Debug)]
 #[structopt(name = "mir")]
 struct CliArgs {
@@ -18,19 +21,66 @@ struct CliArgs {
     /// GitLab remote host
     #[structopt(short = "H", long = "host", default_value = "gitlab.com")]
     host: String,
-    /// Verbose mode (-v, -vv, -vvv, etc.)
-    #[structopt(short = "v", long = "verbose", parse(from_occurrences))]
-    verbose: u8,
+    /// GitLab personal access token
+    #[structopt(short = "p", long = "personal-access-token")]
+    personal_access_token: Option<String>,
+    // /// Verbose mode (-v, -vv, -vvv, etc.)
+    // #[structopt(short = "v", long = "verbose", parse(from_occurrences))]
+    // verbose: u8,
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = CliArgs::from_args();
-    let password = rpassword::prompt_password_stdout("Enter GitLab personal access token: ")?;
-    let gitlab = Gitlab::new(&args.host, password)?;
+    let password = match args.personal_access_token {
+        Some(p) => p,
+        None => rpassword::prompt_password_stdout("Enter GitLab personal access token: ")?
+    };
+    let gitlab = Gitlab::new(&args.host, &password)?;
+    let user = gitlab.current_user()?;
     let projects = gitlab.owned_projects()?;
-    projects.iter().map(|ref p| &p.namespace.full_path).collect::<HashSet<_>>().iter().for_each(|n| {
+    let namespaces = projects.iter().map(|ref p| &p.namespace.full_path).collect::<HashSet<_>>();
+    for n in namespaces {
         let namespace = format!("{}/{}", args.destination, n);
-        fs::create_dir_all(&namespace).expect(&format!("failed to create the directory '{}'", n));
-    });
+        println!("mkdir '{}'", namespace);
+        fs::create_dir_all(&namespace)?;
+    }
+    if args.clone {
+        let progress_bar = ProgressBar::new(0);
+        progress_bar.set_style(ProgressStyle::default_bar()
+            .template("[{elapsed_precise}] {msg:18}: {bar:40.magenta/red} {pos}/{len}")
+            .progress_chars("##-"));
+        let mut remote_callbacks = RemoteCallbacks::new();
+        remote_callbacks
+            .credentials(|_, _, credential_type| {
+                match credential_type {
+                    CredentialType::USER_PASS_PLAINTEXT => Cred::userpass_plaintext(&user.username, &password),
+                    _ => Err(Error::from_str(&format!("Unsupported requested credential type '{:?}'", credential_type))),
+                }
+            })
+            .transfer_progress(|p| {
+                if p.received_objects() < p.total_objects() {
+                    progress_bar.set_message("Receiving objects");
+                    progress_bar.set_length(p.total_objects() as u64);
+                    progress_bar.set_position(p.received_objects() as u64);
+                } else {
+                    progress_bar.set_message("Resolving deltas");
+                    progress_bar.set_length(p.total_deltas() as u64);
+                    progress_bar.set_position(p.indexed_deltas() as u64);
+                }
+                true
+            });
+        let mut fetch_options = FetchOptions::new();
+        fetch_options.remote_callbacks(remote_callbacks);
+        let mut repo_builder = RepoBuilder::new();
+        repo_builder.fetch_options(fetch_options);
+        for p in projects {
+            let namespace = format!("{}/{}/{}", args.destination, p.namespace.full_path, p.name);
+            progress_bar.println(format!("Cloning into '{}'...", namespace));
+            fs::create_dir_all(&namespace).expect(&format!("failed to create the directory '{}'", namespace));
+            repo_builder.clone(&p.http_url_to_repo, Path::new(&namespace))?;
+            progress_bar.finish();
+        }
+        progress_bar.finish_and_clear();
+    }
     Ok(())
 }
