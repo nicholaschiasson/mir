9fde2757b1c1edcfc8dde8dc3c5246c4639cb986
Nicholas Omer Chiasson <nicholasomerchiasson@gmail.com>
2019-07-21 01:04:46 -0400

    Well this could break everything but here goes...

---
+++ version.sh
@@ -0,0 +1,17 @@
+VERSION=${VERSION:-0.0.0}
+
+case "${1}" in
+	master)
+		if git log --format=%s -n 1 | grep -i "Merge branch 'major/" > /dev/null; then
+				export VERSION_BUMP=major
+			elif git log --format=%s -n 1 | grep -i "Merge branch 'minor/" > /dev/null; then
+				export VERSION_BUMP=minor
+			else
+				export VERSION_BUMP=patch
+			fi
+		echo $(semver -i ${VERSION_BUMP} ${VERSION%%-*})
+		;;
+	branches|tags|*)
+		echo ${VERSION}
+		;;
+esac
